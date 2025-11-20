91e93e3cf0f6581e0ce4c75d9df2f6fe9a7f4353
Juangranda3424 <juan.granda3424@gmail.com>
2025-11-20 01:18:02 -0500

    delete crud student for change current

--- Unidad 2/crudestudiante/src/main/java/ec/edu/espe/logica_negocio/EstudianteService.java
+++
@@ -1,5 +0,0 @@
-package ec.edu.espe.logica_negocio;
-
-public class EstudianteService {
-
-}
--- Unidad 2/crudestudiante/src/main/java/ec/edu/espe/presentacion/Main.java
+++
@@ -1,7 +0,0 @@
-package ec.edu.espe.presentacion;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
